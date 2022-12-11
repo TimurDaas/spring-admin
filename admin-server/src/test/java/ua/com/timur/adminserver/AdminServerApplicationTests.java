c9c64ea657176ffa0512147014f334a2c6272492
timur.daas <timur.daas@csltd.com.ua>
2022-12-12 01:34:01 +0200

    test profile

--- admin-server/src/test/java/ua/com/timur/adminserver/AdminServerApplicationTests.java
+++ admin-server/src/test/java/ua/com/timur/adminserver/AdminServerApplicationTests.java
@@ -2,8 +2,10 @@ package ua.com.timur.adminserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AdminServerApplicationTests {
 
     @Test
